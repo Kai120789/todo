37cabfbc38489ce72f0f300af24ca69a47281364
Kai120789 <zoomer120789@gmail.com>
2024-10-06 14:36:50 +0300

    feat: migrator fix

--- cmd/migrator/migrator.go
+++ cmd/migrator/migrator.go
@@ -18,6 +18,7 @@ func main() {
 
 	dbDSN := os.Getenv("DBDSN")
 
+	// reedit string for use migrations out of docker
 	migrateDsn := dbDSN[:27] + "localhost:5431/taskdb?sslmode=disable"
 
 	fmt.Println(migrateDsn)
@@ -28,50 +29,32 @@ func main() {
 	}
 
 	var direction string
-	flag.StringVar(&direction, "d", "", "direction of migration: 'down' or 'up'")
+	flag.StringVar(&direction, "d", "", "direction of migration: 'down' or 'up'") // flag for up or down migrations
 	flag.Parse()
 
 	if direction == "" {
-		err = MigrateUp(db, "./migrations", "up")
+		err = Migrate(db, "./migrations", "up")
 		if err != nil {
 			return
 		}
 	} else if direction == "down" {
-		err = MigrateDown(db, "./migrations", "down")
+		err = Migrate(db, "./migrations", "down")
 		if err != nil {
 			return
 		}
 	}
 
-	fmt.Println("Миграции успешно применены!")
+	fmt.Println("Migrations done!")
 }
 
-func MigrateUp(db *pgxpool.Pool, migrationPath string, direction string) error {
+func Migrate(db *pgxpool.Pool, migrationPath string, direction string) error {
 	files, err := os.ReadDir(migrationPath)
 	if err != nil {
 		return err
 	}
 
 	for _, file := range files {
-		if strings.HasSuffix(file.Name(), "up.sql") {
-			sqlFilePath := filepath.Join(migrationPath, file.Name())
-			err := executeMigration(db, sqlFilePath)
-			if err != nil {
-				return err
-			}
-		}
-	}
-
-	return nil
-}
-func MigrateDown(db *pgxpool.Pool, migrationPath string, direction string) error {
-	files, err := os.ReadDir(migrationPath)
-	if err != nil {
-		return err
-	}
-
-	for _, file := range files {
-		if strings.HasSuffix(file.Name(), ".down.sql") {
+		if strings.HasSuffix(file.Name(), fmt.Sprintf(".%s.sql", direction)) {
 			sqlFilePath := filepath.Join(migrationPath, file.Name())
 			err := executeMigration(db, sqlFilePath)
 			if err != nil {
@@ -93,7 +76,7 @@ func executeMigration(db *pgxpool.Pool, sqlFilePath string) error {
 
 	_, err = db.Exec(context.Background(), string(schemaSQL))
 	if err != nil {
-		fmt.Printf("Ошибка при выполнении миграции %s: %v\n", sqlFilePath, err) // Логирование ошибок
+		fmt.Printf("Migrate error %s: %v\n", sqlFilePath, err)
 		return err
 	}
 
